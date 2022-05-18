7da2f8c4d106903d73588f746f6e014fea2d0720
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-18 18:30:41 +0000

    add the main files

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/master.css">
+    <title>Calander</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-dark bg-primary nb-3">
+      <a href="/index.php" class="navbar-brand">My Calander</a>
+    </nav>
+    <h1>Mars 2022</h1>
+  </body>
+</html>
