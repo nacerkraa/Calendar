1a25af9cb3e0fd1968fcfb61befb23cf9fc037f2
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-05-18 23:06:56 +0000

    styling the table

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Calander</title>
   </head>
   <body>
@@ -15,11 +15,11 @@
       $mounth = new App\Date\Mounth($_GET['mounth'] ?? null,$_GET['year'] ?? null);
 
     ?>
-
+    <h1>nacer kraa</h1>
     <?php echo $mounth->toString(). "<br><br>" ?>
     <?php echo $mounth->getWeeks(); ?>
 
-    <table>
+    <table class="calander-table">
       <?php for ($i=0; $i < $mounth -> getWeeks() ; $i++):?>
         <tr>
           <td>Lundi</td>
